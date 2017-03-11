21316eb049e0924d45ca0270cf9d4f577a4f81a3
donghiep1995@gmail.com <donghiep1995@gmail.com>
2017-03-11 22:01:27 +0700

    upload

---
+++ app/src/main/java/chat/hdd/smartbird/whoareyou/Controller/Adapter_Friend.java
@@ -0,0 +1,8 @@
+package chat.hdd.smartbird.whoareyou.Controller;
+
+/**
+ * Created by hiepdd on 11/03/2017.
+ */
+
+public class Adapter_Friend {
+}
---
+++ app/src/main/java/chat/hdd/smartbird/whoareyou/Model/Friend.java
@@ -0,0 +1,8 @@
+package chat.hdd.smartbird.whoareyou.Model;
+
+/**
+ * Created by hiepdd on 11/03/2017.
+ */
+
+public class Friend {
+}
